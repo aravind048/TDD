b0f8735de726491416fdebbc613ce98a68b33ad1
aravind048 <59740040+aravind048@users.noreply.github.com>
2024-08-24 13:32:42 +0530

    Add files via upload
    Addition:
    Handle different delimiters

--- calculator.js
+++ calculator.js
@@ -3,7 +3,14 @@ function add(numbers) {
         return 0;
     }
 
-    const numsArray = numbers.split(/[\n,]/);
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split('\n');
+        delimiter = new RegExp(`[${parts[0].slice(2)}]`);
+        numbers = parts[1];
+    }
+
+    const numsArray = numbers.split(delimiter);
     return numsArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
--- calculator.test.js
+++ calculator.test.js
@@ -20,3 +20,7 @@ test('should handle new lines between numbers', () => {
     expect(add("1\n2,3")).toBe(6);
 });
 
+test('should support different delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+});
+
