ac83daffbb647d7b378ca694f31868734700f1c0
aravind048 <59740040+aravind048@users.noreply.github.com>
2024-08-24 12:43:06 +0530

    Add files via upload
    Addition: 
    Return same number for single string

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const add = require('./calculator');
 test('should return 0 for an empty string', () => {
     expect(add("")).toBe(0);
 });
+
+test('should return the number itself for a single number', () => {
+    expect(add("1")).toBe(1);
+});
