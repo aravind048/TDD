3a11bc7f2d7679f95f55ebebe39b4ae86b729afb
aravind048 <59740040+aravind048@users.noreply.github.com>
2024-08-24 12:47:41 +0530

    Add files via upload
    Addition: 
    Sum of two numbers

--- calculator.js
+++ calculator.js
@@ -2,7 +2,8 @@ function add(numbers) {
     if (numbers === "") {
         return 0;
     }
-    return parseInt(numbers, 10);
+    const numsArray = numbers.split(",");
+    return numsArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
 module.exports = add;
--- calculator.test.js
+++ calculator.test.js
@@ -7,3 +7,7 @@ test('should return 0 for an empty string', () => {
 test('should return the number itself for a single number', () => {
     expect(add("1")).toBe(1);
 });
+
+test('should return the sum of two numbers', () => {
+    expect(add("1,2")).toBe(3);
+});
